99b6ea94b50ebe2852912cde321ab7fb42fb9248
Aleksandras Sivkovas <aleksandras.sivkovas@gmail.com>
2018-04-01 19:36:03 +0300

    exporting components from modeling-storage

--- src/main/js/modeling-storage/index.js
+++ src/main/js/modeling-storage/index.js
@@ -1,3 +1,7 @@
-export {default as Storage} from "./Storage.js";
-export {default as Converter} from "./Converter.js";
+export {default as EdgeConverter} from "./component-converters/EdgeConverter.js";
+export {default as NodeConverter} from "./component-converters/NodeConverter.js";
+export {default as RelationConverter} from "./component-converters/RelationConverter.js";
+export {default as ComponentConverter} from "./component-converters/ComponentConverter.js";
 export {default as ModelConverter} from "./ModelConverter.js";
+export {default as Converter} from "./Converter.js";
+export {default as Storage} from "./Storage.js";
