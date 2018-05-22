7a72d2e54ad4a4101b3afa4ba6329e43834086f5
Aleksandras Sivkovas <aleksandras.sivkovas@gmail.com>
2018-05-22 15:44:29 +0300

    removing mof

--- src/main/js/mof/index.js
+++
@@ -1,6 +0,0 @@
-// Primitive types package
-export {default as Integer} from './primitive-types/Integer.js';
-export {default as Boolean} from './primitive-types/Boolean.js';
-export {default as String} from './primitive-types/String.js';
-export {default as UnlimitedNatural} from './primitive-types/UnlimitedNatural.js';
-export {default as Real} from './primitive-types/Real.js';
